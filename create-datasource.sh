c62960ee24e01a63111c1103fbcc53457c9973b3
venkatesh <venkatesh251997@gmail.com>
2020-09-03 22:24:39 +0530

    config

--- create-datasource.sh
+++ create-datasource.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+chown -R nobody:nogroup .
+
 # Loop until the post succeeds - handles the case where Grafana conatiner is live but not ready
 until curl -s -H "Content-Type: application/json" -X POST -d '{"name":"prometheus-dev","type":"prometheus","url":"http://prometheus:9090","access":"proxy"}' http://admin:password@localhost:3000/api/datasources 2>&1 > /dev/null;
 do
